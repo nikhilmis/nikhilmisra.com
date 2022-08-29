8e6a21a3a44d41bf7c8d9adfbb5178de40f713b1
OleSkaar <olejohska@gmail.com>
2022-08-29 19:12:51 +0200

    Set up GitHub pages deploy action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+    site: 'https://oleskaar.github.io',
+    integrations: [mdx()],
 });
