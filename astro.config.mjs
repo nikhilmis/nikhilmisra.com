ffd902995a4d632205266d24e725f13f3e811faf
OleSkaar <olejohska@gmail.com>
2022-08-29 19:31:50 +0200

    Revert "Set base"
    This reverts commit 50e5332a012754c77a3d2b36c31c82803bce75e2.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://oleskaar.github.io',
-    base: '/nikhilmisra.com-astro',
     integrations: [mdx()],
 });
