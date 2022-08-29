d44ac418469f94dbb2cd9eda6b038cb40cce87d4
OleSkaar <olejohska@gmail.com>
2022-08-29 20:10:12 +0200

    Install Image components, test images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://oleskaar.github.io',
-    integrations: [mdx()],
+  site: 'https://oleskaar.github.io',
+  integrations: [mdx(), image()]
 });
