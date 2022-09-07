f30891df7ca6f31a71b7f3c3c2eaf9b857dca0d2
OleSkaar <olejohska@gmail.com>
2022-09-07 20:21:30 +0200

    Update astro config with right domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oleskaar.github.io',
+  site: 'https://www.nikhilmisra.com',
   integrations: [mdx(), image(), svelte()]
 });
