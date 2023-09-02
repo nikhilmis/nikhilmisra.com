00f25c2c010d798f4c389ce06f4d5d500c369139
OleSkaar <olejohska@gmail.com>
2023-09-02 17:41:48 +0200

    Use built-in image service, new picture component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nikhilmisra.com',
-    integrations: [mdx(), image({ serviceEntryPoint: '@astrojs/image/sharp' }), svelte()],
+    integrations: [mdx(), svelte()],
 });
