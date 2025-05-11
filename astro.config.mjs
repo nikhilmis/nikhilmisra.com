b5f2420a32402171e3106247770918dcf58eae83
OleSkaar <olejohska@gmail.com>
2025-05-11 22:16:31 +0200

    No www

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), mdx()],
-  site: 'https://www.nikhilmisra.com'
+  site: 'https://nikhilmisra.com'
 });
